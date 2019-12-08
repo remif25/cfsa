c7eb99912ff1551f9f96b8bac19e710152f67001
Rémi Fongaufier <admin@remi-fongaufier.ch>
2019-12-08 22:42:52 +0100

    WIP  : configurateur

--- assets/js/configurateur.js
+++ assets/js/configurateur.js
@@ -4,13 +4,19 @@ require('bootstrap/js/dist/modal');
 import '../scss/main.scss'
 
 $(document).ready(function() {
+
     $('.has-regle').hover(function() {
         let id_regle = $(this).data('regle_id');
-        $('.regle_id-' + id_regle).toggleClass('selected-regle');
+        $('.regle_id-' + id_regle).addClass('selected-regle');
+    });
+    $('.has-regle').mouseout(function() {
+        let id_regle = $(this).data('regle_id');
+        $('.regle_id-' + id_regle).removeClass('selected-regle');
     });
 
     $('.has-regle').click(function() {
         let id_regle = $(this).data('regle_id');
         $('#modalRegle' + id_regle).modal();
     });
+    $('.has-regle').removeClass('selected-regle');
 });
